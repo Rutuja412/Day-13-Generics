34cc596eaa69afa67e911f0000df6d3e34af91b3
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-12 22:43:09 +0530

    [Rutuja]=Adding UC1 Generics

---
+++ UC1Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UC1Array
+{
+    class program
+    {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+             Console.WriteLine(element);
+            }
+            Console.WriteLine("--------");
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element); 
+            }
+            Console.WriteLine("-------------");
+        }
+        public static void toPrint(char[] inputArray)
+        {
+            foreach(char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------");
+
+        }
+        public void Main(string[] args) 
+        {
+            int[] inputArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+
+            program.toPrint(inputArray);
+            program.toPrint(doubleArray);
+            program.toPrint(charArray);
+        }
+    }
+}
+
+
