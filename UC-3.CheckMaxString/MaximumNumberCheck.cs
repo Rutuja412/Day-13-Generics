9207cb85aa95e037d9a39b9e61a83e2cc241f213
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 16:26:30 +0530

    [Rutuja]=To check maximum String

---
+++ UC-3.CheckMaxString/MaximumNumberCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_3.CheckMaxString
+{
+     class MaximumNumberCheck
+    {
+        public static string MaximumStringNumber(string firststring, string secondstring, string thirdstring)
+        {
+            if (firststring.CompareTo(secondstring) > 0 && firststring.CompareTo(thirdstring) > 0) 
+                
+            {
+                return firststring;
+            }
+            if (secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) > 0)
+            {
+                return secondstring;
+            }
+            if (thirdstring.CompareTo(firststring) > 0 && thirdstring.CompareTo(secondstring) > 0 )
+            {
+                return thirdstring;
+            }
+            throw new Exception("firstNumber,second Number & third Number are same");
+        }
+    }
+}
---
+++ UC-3.CheckMaxString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC_3.CheckMaxString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max String");
+            string stringOutput = MaximumNumberCheck.MaximumStringNumber("Apple", "banana", "pineapple");
+            Console.WriteLine(stringOutput);
+            
+        }
+    }
+}
