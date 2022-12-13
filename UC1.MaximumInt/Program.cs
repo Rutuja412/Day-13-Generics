cec53f04c8b5393effa81e5fdaa900543a443358
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 15:45:36 +0530

    [Rutuja]=To check maximum integer

---
+++ UC1.MaximumInt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC1.MaximumInt
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum number");
+
+            int output = MaximumNumberCheck.MaximumIntegerNumber(100, 200, 300);
+            Console.WriteLine(output);
+        }
+    }
+}
